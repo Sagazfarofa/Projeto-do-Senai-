df19d372249e1cbb892b58be1f2d63d2b16ed4ad
Luiz Gustavo <luiz.g.rosa9@aluno.senai.br>
2026-06-17 15:38:03 -0300

    projetão

---
+++ backend/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    senha = Column(String)
+    nome = Column(String)
+    materia = Column(String)
